50da9f7fd4ae148c5fbcb67b5045ea3d0c173f8b
Abumahfouz <mahfoozabubakar@rocketmail.com>
2022-08-10 11:47:12 +0100

    Update Program.cs

--- DeterminantOfFourByFour/Program.cs
+++ DeterminantOfFourByFour/Program.cs
@@ -4,41 +4,24 @@
     {
         static void Main(string[] args)
         {
-             Console.WriteLine("The Determinant of a Four By Four Matrix!");
-            int[,] fourDimensionArray = new int[4, 4]
+            Console.WriteLine("The Determinant of a Four By Four Matrix!");
+            int[,] D = new int[4, 4]
            {
                 {1,1,1,-1 },
                 { 1,1,-1,1},
                 { 1,-1,1,1},
                 {-1,1,1,1 },
            };
+            
 
-            int cell1 = fourDimensionArray[0, 0];
-            int cell2 = fourDimensionArray[0, 1];
-            int cell3 = fourDimensionArray[0, 2];
-            int cell4 = fourDimensionArray[0, 3];
-            int cell5 = fourDimensionArray[1, 0];
-            int cell6 = fourDimensionArray[1, 1];
-            int cell7 = fourDimensionArray[1, 2];
-            int cell8 = fourDimensionArray[1, 3];
-            int cell9 = fourDimensionArray[2, 0];
-            int cell10 = fourDimensionArray[2, 1];
-            int cell11 = fourDimensionArray[2, 2];
-            int cell12 = fourDimensionArray[2, 3];
-            int cell13 = fourDimensionArray[3, 0];
-            int cell14 = fourDimensionArray[3, 1];
-            int cell15 = fourDimensionArray[3, 2];
-            int cell16 = fourDimensionArray[3, 3];
 
-            int determinant = cell1 * cell6 * cell11 * cell16 + cell1 * cell7 * cell12 * cell14 + cell1 * cell8 * cell10 * cell15
-                            - cell1 * cell8 * cell11 * cell14 - cell1 * cell7 * cell10 * cell16 - cell1 * cell6 * cell12 * cell15
-                            - cell2 * cell5 * cell11 * cell16 - cell3 * cell5 * cell12 * cell14 - cell4 * cell5 * cell10 * cell15
-                           + cell4 * cell5 * cell11 * cell14 + cell3 * cell5 * cell10 * cell16 + cell2 * cell5 * cell12 * cell15
-                           + cell2 * cell7 * cell9 * cell16 + cell3 * cell8 * cell9 * cell14 + cell4 * cell6 * cell9 * cell15
-                           - cell4 * cell7 * cell9 * cell14 - cell3 * cell6 * cell9 * cell16 - cell2 * cell8 * cell9 * cell15
-                           - cell2 * cell7 * cell12 * cell13 - cell3 * cell8 * cell10 * cell13 - cell4 * cell6 * cell11 * cell13
-                           + cell4 * cell7 * cell10 * cell13 + cell3 * cell6 * cell12 * cell13 + cell2 * cell8 * cell11 * cell13;
-            Console.WriteLine(determinant);
+
+            int determinant = D[0, 0] * (D[1, 1] * ((D[2, 2] * D[3, 3]) - (D[3, 2] * D[2, 3])) - D[1, 2] * ((D[2, 1] * D[3, 3]) - (D[3, 1] * D[2, 3])) + D[1, 3] * ((D[2, 1] * D[3, 2]) - (D[3, 1] * D[2, 2])))
+                            - D[0, 1] * (D[1, 0] * ((D[2, 2] * D[3, 3]) - (D[3, 2] * D[2, 3])) - D[1, 2] * ((D[2, 0] * D[3, 3]) - (D[3, 0] * D[2, 3])) + D[1, 3] * ((D[2, 0] * D[3, 2]) - (D[3, 0] * D[2, 2])))
+                            + D[0, 2] * (D[1, 0] * ((D[2, 1] * D[3, 3]) - (D[3, 1] * D[2, 3])) - D[1, 1] * ((D[2, 0] * D[3, 3]) - (D[3, 0] * D[2, 3])) + D[1, 3] * ((D[2, 0] * D[3, 1]) - (D[3, 0] * D[2, 1])))
+                            - D[0, 3] * (D[1, 0] * ((D[2, 1] * D[3, 2]) - (D[3, 1] * D[2, 2])) - D[1, 1] * ((D[2, 0] * D[3, 2]) - (D[3, 0] * D[2, 2])) + D[1, 2] * ((D[2, 0] * D[3, 1]) - (D[3, 0] * D[2, 1])));
+            Console.WriteLine("\n");
+            Console.WriteLine("\n The determinant is " + determinant);
         }
     }
 }
